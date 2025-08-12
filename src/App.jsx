506f3dbecbf60162bfd3795de37aa19fe173642b
Jez Cortez <74903241+JezCortez@users.noreply.github.com>
2025-08-12 12:27:14 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import ForgotPassword from './pages/ForgotPassword'
-import ResetPassword from './pages/ResetPassword'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import ResetPassword from "./pages/ResetPassword";
 
-export default function App() {
+function App() {
   return (
     <Router>
       <Routes>
-        {/* Default route */}
-        <Route path="/" element={<Navigate to="/login" />} />
-
-        {/* Auth routes */}
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-
-        {/* App pages */}
         <Route path="/dashboard" element={<Dashboard />} />
-
-        {/* Catch-all */}
-        <Route path="*" element={<h1 className="text-center mt-20">404 - Page Not Found</h1>} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </Router>
-  )
+  );
 }
+
+export default App;
